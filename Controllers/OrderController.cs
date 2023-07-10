179d8b3e83e7e88323c81002087f0200dac1f613
jvolive <joaooliveira1999@outlook.com>
2023-07-10 16:12:20 -0300

    finish OrderController

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -17,7 +17,8 @@ namespace Advantage.API.Controllers
         [HttpGet("{pageIndex:int}/{pageSize:int}")]
         public IActionResult Get(int pageIndex, int pageSize)
         {
-            var data = _ctx.Orders.Include(o => o.Customer).OrderByDescending(c => c.Placed);
+            var data = _ctx.Orders.Include(o => o.Customer)
+            .OrderByDescending(c => c.Placed);
 
             var page = new PaginatedResponse<Order>(data, pageIndex, pageSize);
 
@@ -33,5 +34,46 @@ namespace Advantage.API.Controllers
             return Ok(response);
         }
 
+        [HttpGet("ByState")]
+        public IActionResult ByState()
+        {
+            var orders = _ctx.Orders.Include(o => o.Customer).ToList();
+
+            var groupedResult = orders.GroupBy(o => o.Customer.State)
+            .ToList()
+            .Select(grp => new
+            {
+                State = grp.Key,
+                Total = grp.Sum(x => x.Total)
+            }).OrderByDescending(res => res.Total)
+            .ToList();
+
+            return Ok(groupedResult);
+        }
+
+        [HttpGet("ByCustomer/{n}")]
+        public IActionResult ByCustomer(int n)
+        {
+            var orders = _ctx.Orders.Include(o => o.Customer).ToList();
+
+            var groupedResult = orders.GroupBy(o => o.Customer.Id)
+            .ToList()
+            .Select(grp => new
+            {
+                Name = _ctx.Customers.Find(grp.Key).Name,
+                Total = grp.Sum(x => x.Total)
+            }).OrderByDescending(res => res.Total)
+            .Take(n)
+            .ToList();
+
+            return Ok(groupedResult);
+        }
+
+        [HttpGet("GetOrder/{id}", Name = "GetOrder")]
+        public IActionResult GetOrder(int id)
+        {
+            var order = _ctx.Orders.Include(o => o.Customer).First(o => o.Id == id);
+            return Ok(order);
+        }
     }
 }
